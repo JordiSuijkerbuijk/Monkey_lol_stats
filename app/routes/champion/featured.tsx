e6dfe67160c0f2bcf7be3fcf6243cfa8ebf17a94
Jordi Suijkerbuijk <j.suijkerbuijk@hotmail.com>
2022-03-11 15:03:22 +0100

    Added free champion rotation detail page

---
+++ app/routes/champion/featured.tsx
@@ -0,0 +1,31 @@
+import { useLoaderData, LoaderFunction } from 'remix';
+import fetch from '../../utils/fetch';
+
+import type { Champion } from '../../types/champion';
+
+type LoaderData = {
+  data: boolean | Array<Champion>;
+};
+
+export const loader: LoaderFunction = async () => {
+  const data = await fetch('http://localhost:3000/champion/rotation');
+
+  return data;
+};
+
+export default function Featured() {
+  const data = useLoaderData<LoaderData>();
+
+  const featuredChampions = (data.data as Array<Champion>) || false;
+
+  console.log('data', featuredChampions);
+
+  return (
+    <main className='grid gap-12'>
+      {featuredChampions &&
+        featuredChampions.map((item: Champion) => <div key={item.key}> {item.id}</div>)}
+    </main>
+  );
+}
+
+//
